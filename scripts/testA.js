108587eea9fa3c3047a5af22c240c69919549630
이정수 <jslee3@suprema.co.kr>
2022-07-25 13:22:26 +0900

    init

---
+++ scripts/testA.js
@@ -0,0 +1,5 @@
+import {b} from "./testB.js";
+
+console.log(b)
+const a = 2;
+
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: path.join(__dirname + "/main.js"),
+    output: {
+        path: path.join(__dirname + "/build"),
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            use: ['babel-loader'],
+            exclude: /node_modules/
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname + "/index.html")
+        }),
+    ],
+    devServer: {
+        port: 3000,
+        hot: true,
+        host: 'localhost',
+        static: {
+            directory: path.join(__dirname),
+        },
+    }
+}
